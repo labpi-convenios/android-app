5501a312c4686b2067ffbd41594b87a89f9fdde0
Gabriel Silva <gabriel93.silva@gmail.com>
2016-03-29 22:50:16 -0300

    Adds project structure
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

---
+++ app/src/main/java/gabrielssilva/androidapp/MainActivity.java
@@ -0,0 +1,13 @@
+package gabrielssilva.androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
