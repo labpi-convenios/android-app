38866180ad72217a7c53ff75cf5ead48641448bc
Gabriel Silva <gabriel93.silva@gmail.com>
2016-04-09 19:53:36 -0300

    Turns TabbedACtivity into MainActivity and adds TabLayout
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- app/src/main/java/gabrielssilva/androidapp/MainActivity.java
+++ app/src/main/java/gabrielssilva/androidapp/MainActivity.java
@@ -1,7 +1,13 @@
 package gabrielssilva.androidapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import gabrielssilva.adapter.TabsAdapter;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabsAdapter mTabsAdapter = new TabsAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) this.findViewById(R.id.container);
+        viewPager.setAdapter(mTabsAdapter);
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) this.findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
     }
 }
